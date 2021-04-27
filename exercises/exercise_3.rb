d195145d5ab9cf1539198e4a67595e23c4fc6498
Paul Ladd <vlendle@gmail.com>
2021-04-27 02:34:15 +0000

    Add exercise 3 implementation

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3);
+
+# There should be three stores. That's one store too many.
+puts Store.count
+
+# Burn it down.
+@store3.destroy
+
+# Make sure it's dead.
+puts Store.count
