95b658e1a695305cc75ace3621713cd94194118d
Paul Ladd <vlendle@gmail.com>
2021-04-27 17:50:51 +0000

    Add user input of store data with error message printing

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+print "> "
+store_name = $stdin.gets.chomp
+
+puts "Carries men's clothing? [Y/N]:"
+print "> "
+mens_apparel = $stdin.gets.chomp.downcase == "y"
+
+puts "Carries women's clothing? [Y/N]:"
+print "> "
+womens_apparel = $stdin.gets.chomp.downcase == "y"
+
+puts "Enter annual revenue:"
+print "> "
+annual_revenue = $stdin.gets.chomp.to_i
+
+result = Store.create(
+  name: store_name,
+  mens_apparel: mens_apparel,
+  womens_apparel: womens_apparel,
+  annual_revenue: annual_revenue
+  )
+
+result.errors && result.errors.full_messages.each {|msg| puts msg}
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates_associated :employees
+  validate :must_carry_clothing_line
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: -1}
+
+  def must_carry_clothing_line
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:base, "store must carry at least one line of apparel")
+    end
+  end
 end
